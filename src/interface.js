f1d2834369ac830714e3998f766319b6f72578e8
Toby Hamand <tobyhamand@gmail.com>
2017-08-03 17:08:06 +0100

    added lots of stuff

--- src/interface.js
+++ src/interface.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  var powerusagehash = {  'low-usage' : 'green',
-                          'medium-usage' : 'yellow',
-                          'high-usage' : 'red'}
+  var powerusagehash = {  'low-usage' : '#9BC53D',
+                          'medium-usage' : '#FDE74C',
+                          'high-usage' : '#E55934'};
+
+  var powersavehash = {  true : 'green',
+                          false : 'red'};
 
   function updateDisplays() {
-    $('#powersavetoggledisplay').text(thermostat._powersaving);
-    $('#energyusagedisplay').text(thermostat.getEnergyUsage());
     $("#tempdisplay").css("background-color", powerusagehash[thermostat.getEnergyUsage()]);
     $('#tempdisplay').text(thermostat.getTemp());
   }
 
+  function updateWeather() {
+    weatherapi = 'http://api.openweathermap.org/data/2.5/weather?q=' + $('#cityselector').find(":selected").text() + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric'
+      $.get(weatherapi, function(data) {
+        $('#temperature').text((data.main.temp).toFixed(0) + ' °C');
+        $('#weather').text(data.weather[0].main);
+        $('#weatherpicture').html('<img src="../assets/weather/' + data.weather[0].main + '.png" height=17%>');
+        $('#location').text($('#cityselector').find(":selected").text());
+      })
+  }
+
   updateDisplays();
-
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed', function(data) {
-    $('#temperature').text(data.main.temp - 273.15);
-  })
-
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed', function(data) {
-    $('#weather').text(data.weather[0].description);
-  })
+  updateWeather();
 
   $('#increasetempbutton').click(function() {
     thermostat.increaseTemp(1);
@@ -36,8 +40,14 @@ $(document).ready(function() {
     updateDisplays();
   });
 
-  $('#powersavetogglebutton').click(function() {
+  $(document).on('click','#powersavetogglebutton', function() {
     thermostat.powerSaveToggle();
     updateDisplays();
+  });
+
+  $('#cityselector').change(function() {
+    $('#location').text($('#cityselector').find(":selected").text());
+    updateWeather();
+    updateWeatherDisplay();
   });
 })
