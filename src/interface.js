f26ba0d1cc78a5761b75a84d4d3dbb9ba00701c0
Toby Hamand <tobyhamand@gmail.com>
2017-08-04 15:11:09 +0100

    added basic sinatra architecture

--- src/interface.js
+++ src/interface.js
@@ -12,14 +12,20 @@ $(document).ready(function() {
     $('#tempdisplay').text(thermostat.getTemp());
   }
 
+  function sentPost() {
+    var json = JSON.stringify({ temp: thermostat.getTemp(), city: $('#cityselector').find(":selected").text()});
+    console.log(json)
+    $.post('localhost:9292', json)
+  }
+
   function updateWeather() {
-    weatherapi = 'http://api.openweathermap.org/data/2.5/weather?q=' + $('#cityselector').find(":selected").text() + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric'
+    weatherapi = 'http://api.openweathermap.org/data/2.5/weather?q=' + $('#cityselector').find(":selected").text() + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric';
       $.get(weatherapi, function(data) {
         $('#temperature').text((data.main.temp).toFixed(0) + ' °C');
         $('#weather').text(data.weather[0].main);
-        $('#weatherpicture').html('<img src="../assets/weather/' + data.weather[0].main + '.png" height=17%>');
+        $('#weatherpicture').html('<img src="../assets/weather/' + data.weather[0].main + '.png" height=210>');
         $('#location').text($('#cityselector').find(":selected").text());
-      })
+      });
   }
 
   updateDisplays();
@@ -28,26 +34,30 @@ $(document).ready(function() {
   $('#increasetempbutton').click(function() {
     thermostat.increaseTemp(1);
     updateDisplays();
+    sentPost()
   });
 
   $('#decreasetempbutton').click(function() {
     thermostat.decreaseTemp(1);
     updateDisplays();
+    sentPost()
   });
 
   $('#resettempbutton').click(function() {
     thermostat.resetTemp();
     updateDisplays();
+    sentPost()
   });
 
   $(document).on('click','#powersavetogglebutton', function() {
     thermostat.powerSaveToggle();
     updateDisplays();
+    sentPost()
   });
 
   $('#cityselector').change(function() {
     $('#location').text($('#cityselector').find(":selected").text());
     updateWeather();
-    updateWeatherDisplay();
+    sentPost()
   });
 })
